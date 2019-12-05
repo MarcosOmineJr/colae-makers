a234f4c7e63c8a4cb981a38aebe2fb4631f9ea58
Marcos Omine Jr <marcosomine@gmail.com>
2019-12-05 08:40:29 -0300

    Arrumei o nome do app e coloquei os assets certos

--- android/app/src/main/java/com/makers/MainActivity.java
+++ android/app/src/main/java/com/makers/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "makers";
+    return "Colaê PRO";
   }
 
   @Override
